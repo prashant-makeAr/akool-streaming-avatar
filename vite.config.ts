a9825e3ee84a1ce91bba3e8bdfcfe8046114bbd1
prashant <prashant.sharma@makear.ai>
2025-03-02 00:17:29 +0530

    updated homepage path

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,8 @@ import react from '@vitejs/plugin-react-swc'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: process.env.GITHUB_ACTIONS ? '/akool-streaming-avatar-react-demo' : '/streaming/avatar',
+  // base: process.env.GITHUB_ACTIONS ? '/akool-streaming-avatar-react-demo' : '/streaming/avatar',
+  base: "/akool-streaming-avatar/",
   server: {
     host: '0.0.0.0'
   },
